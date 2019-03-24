ecb3cd9fd7e793aa916c15b61f1123358f504030
Anatolii A. A <a3three@gmail.com>
2019-03-24 22:10:01 +0100

    added logging stuff

--- bin/change.py
+++ bin/change.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
     
-import sys, json, argparse, logging
-from pprint import pformat
-    
-version = "1.0.1"
-logger = logging.getLogger('newlogger')
-handler = logging.FileHandler('var/log/changer.log')
 
+import sys, json, argparse, logging, io, os, os.path
+from pprint import pformat
+from os.path import abspath, dirname, join
+
+version = "1.0.1"
+
+BASE_DIR = dirname(dirname(abspath(__file__)))
+APP_CFG = join(BASE_DIR,'data/app-env-conv.json')
+APP_LOG = join(BASE_DIR,'var/log/changer.log')
+
+logger = logging.getLogger('newlogger')
+handler = logging.FileHandler(APP_LOG)
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler) 
+logger.setLevel(logging.INFO)
 
 def createParser ():
     # create Parser class
@@ -25,12 +36,27 @@ def createParser ():
 if __name__ == '__main__':
     p = createParser()
 
+    try:
+        to_unicode = unicode
+    except NameError:
+        to_unicode = str
+
     print('New password [%s] for the env [%s]' % (p.pswd,p.env))
+
+    # logger.info(pformat(APP_LOG))
     
+    # Read JSON file
+    logger.info("Reading JSON file: "+APP_CFG)
+    with open(APP_CFG) as data_file:
+        cfg_data = json.load(data_file)
 
+    # logger.info(pformat(cfg_data))
 
-
-
+    # Write JSON file
+    # with io.open('data.json', 'w', encoding='utf8') as outfile:
+    #     str_ = json.dumps(cfg_data, indent=4, sort_keys=True,
+    #                 separators=(',', ': '), ensure_ascii=False)
+    #     outfile.write(to_unicode(str_))
 
 
 
